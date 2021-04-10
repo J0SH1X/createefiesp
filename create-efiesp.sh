7881c61f37187843d97b221e96f273f9ea713d17
J0SH1X <aljoshua.hell@gmail.com>
2021-04-10 12:38:29 +0200

    inital upload

---
+++ create-efiesp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+BOOT_SIZE=$1
+DIR=mount
+DIR2=esp
+
+#Create the empty image
+dd if=/dev/zero of=efiesp.img bs=1M count=$BOOT_SIZE
+
+#Format as fat32
+mkfs.vfat efiesp.img
+
+#Create the needed folder structure
+if [[ ! -e $DIR ]]; then
+    mkdir $DIR
+elif [[ ! -d $DIR ]]; then
+    echo "$DIR already exists but is not a directory" 1>&2
+fi
+if [[ ! -e $DIR2 ]]; then
+    mkdir $DIR2
+elif [[ ! -d $DIR2 ]]; then
+    echo "$DIR2 already exists but is not a directory" 1>&2
+fi
+
+#Mount the new efiesp image to mountpoint
+sudo mount -o loop,rw efiesp.img ./mount
+
+
+#Wait for user to fill the esp folder with needed files
+read  -n 1 -p "Press any Key to continue" mainmenuinput
+
+#Copy the esp files into the image
+sudo cp -r esp/* mount/
+
+#Cleanup
+sudo umount mount
+rm -rf $DIR
+rm -rf $DIR2
+
